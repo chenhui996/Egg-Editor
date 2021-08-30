c1beb1f6d6e9aa7ab2a83552c5f34d35029bd6f1
cain <chen562450669@qq.com>
2021-08-31 00:16:11 +0800

    feat: add vuex create store

--- egg-editor/src/views/Index.vue
+++ egg-editor/src/views/Index.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="homepage-caontaner">
     <a-layout :style="{background: '#fff'}">
+      <!-- header -->
       <a-layout-header class="header">
         <div class="page-title">
           <router-link to="/">Egg-H5</router-link>
         </div>
       </a-layout-header>
+      <!-- content -->
       <a-layout-content class="home-layout">
         <router-view></router-view>
       </a-layout-content>
+      <!-- footer -->
       <a-layout-footer
         >© EGG（egg.chenhui.com）版权所有 | 沪ICP备xxxxxxxx号-x
       </a-layout-footer>
@@ -17,7 +20,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import {defineComponent} from 'vue'
 export default defineComponent({
   name: 'Index',
 })
