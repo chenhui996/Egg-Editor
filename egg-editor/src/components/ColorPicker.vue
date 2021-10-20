611baf005ecbfe29c14a52fb1b2e72adc42d71d0
cain <chen562450669@qq.com>
2021-10-21 01:21:47 +0800

    feat: TDD dev -> add ColorPicker Unit test

---
+++ egg-editor/src/components/ColorPicker.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="egg-color-picker"></div>
+</template>
+
+<script>
+import {defineComponent} from '@vue/composition-api'
+
+export default defineComponent({})
+</script>
+
+<style></style>
