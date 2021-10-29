3e496949269e30927e378c1a2b174d560b832f42
cain <chen562450669@qq.com>
2021-10-29 17:57:39 +0800

    feat: update unit test -> Uploader component

--- egg-editor/tests/unit/Uploader.spec.ts
+++ egg-editor/tests/unit/Uploader.spec.ts
@@ -24,9 +24,10 @@ describe('Uploader Component', () => {
   it('upload process should works fine', async () => {
     // change e.target.files
     // create a file
-    mockedAxios.post.mockResolvedValueOnce({status: 'success'})
+    mockedAxios.post.mockResolvedValueOnce({status: 'success'}) // mock post -> 发生 post - 基于此定义调用
     const fileInput = wrapper.get('input').element as HTMLInputElement
     const files = [testFile] as any
+    // 由于 unit test 无法模拟 交互的过程 -> 故 input 上传文件操作 -> 直接将文件塞入 input 的 files 属性中。
     Object.defineProperty(fileInput, 'files', {
       value: files,
       writable: false,
@@ -34,8 +35,18 @@ describe('Uploader Component', () => {
     await wrapper.get('input').trigger('change')
     expect(mockedAxios.post).toHaveBeenCalledTimes(1)
     expect(wrapper.get('button span').text()).toBe('正在上传')
+    // button 为 disabled
+    expect(wrapper.get('button span').attributes('disabled')).toBeTruthy()
+
+    // 列表长度修改，并且有正确的 class
+    const firstItem = wrapper.get('li:first-child')
+    expect(firstItem.classes()).toContain('uploading')
+
     await flushPromises()
     expect(wrapper.get('button span').text()).toBe('上传成功')
+    // 有正确的 class，并且文件名称相对应
+    expect(firstItem.classes()).toContain('upload-success')
+    expect(wrapper.get('.filename').text()).toBe(testFile.name)
   })
   it('should return error text when post is rejected', async () => {
     mockedAxios.post.mockRejectedValueOnce({status: 'error'})
@@ -44,5 +55,14 @@ describe('Uploader Component', () => {
     expect(wrapper.get('button span').text()).toBe('正在上传')
     await flushPromises()
     expect(wrapper.get('button span').text()).toBe('上传失败')
+    
+    // 列表长度增加，并且列表的最后一项有正确的 class 名
+    expect(wrapper.findAll('li').length).toBe(2)
+    const lastItem = wrapper.get('li:last-child')
+    expect(lastItem.classes()).toContain('upload-error')
+
+    // 点击列表中右侧的 button，可以删除这一项
+    await lastItem.get('.delete-icon').trigger('click')
+    expect(wrapper.findAll('li').length).toBe(1)
   })
 })
